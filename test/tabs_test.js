343d928fa81632adc0cda47b2ec45cafc76f03ef
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-10-30 16:22:17 +0100

    Landscaper: Migrate to Qunit2

--- test/tabs_test.js
+++ test/tabs_test.js
@@ -8,29 +8,29 @@ F.attach(QUnit);
 
 QUnit.module("bit-tabs view model");
 
-QUnit.test("basics", function(){
+QUnit.test("basics", function(assert) {
 	var tabsVM = new BitTabsVM();
 	var panelVM = new BitPanelVM();
 	tabsVM.addPanel(panelVM);
 
-	equal(panelVM.active, true, "first panel added is active");
+	assert.equal(panelVM.active, true, "first panel added is active");
 });
 
 var template = stache("<bit-tabs tabsClass:from='\"nav nav-tabs\"'><bit-panel title:from='\"First\"'>Hello!</bit-panel><bit-panel title:from='\"Second\"'>Another</bit-panel></bit-tabs>");
 
 QUnit.module("bit-tabs component",{
-	setup: function(){
+	beforeEach: function(assert) {
 		$("#qunit-fixture").append(template());
 	}
 });
 
-QUnit.test("basics", function(){
+QUnit.test("basics", function(assert) {
   F("bit-tabs ul li").text(/First/, "has text");
   F("bit-tabs ul").hasClass("nav", true).hasClass("nav-tabs", true, "tabsClass gets assigned to ul");
   F("bit-tabs ul li").hasClass("active", true, "first tab has active class");
 });
 
-QUnit.test("clicking works", function(){
+QUnit.test("clicking works", function(assert) {
   F("bit-tabs li:nth(1)").click();
   F("bit-panel:nth(1)").text("Another", "Correct text shown");
 });
